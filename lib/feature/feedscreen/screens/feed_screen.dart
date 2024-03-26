b202e52b6f1be209084d9850a8e308c6dde6648b
ANKIT KUMAR DUVEDI <ankitduvedi.03@gmail.com>
2024-03-26 23:01:13 +0530

    Login UI

--- lib/feature/feedscreen/screens/feed_screen.dart
+++ lib/feature/feedscreen/screens/feed_screen.dart
@@ -5,7 +5,6 @@ import 'package:com.example.while_app/feature/notifications/screens/notification
 import 'package:com.example.while_app/feature/feedscreen/controller/categories_test_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class FeedScreen extends ConsumerStatefulWidget {
   const FeedScreen({Key? key}) : super(key: key);
@@ -42,75 +41,86 @@ class FeedScreenState extends ConsumerState<FeedScreen> {
     log("feedscreen");
     final unreadNotifsAsyncValue = ref.watch(listenUnreadNotifsProvider);
     final categoriesState = ref.watch(categoryProvider);
-    // log(categoriesState.categories.toString());
 
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title:
-            Text('WHILE', style: GoogleFonts.ptSans(color: Colors.grey[800])),
-        elevation: 0,
-        backgroundColor: Colors.grey[200],
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 16.0),
-            child: unreadNotifsAsyncValue.when(
-              data: (int unreadCount) {
-                if (unreadCount == 0) {
-                  return IconButton(
-                    icon: const Icon(Icons.notifications_none_rounded,
+    return SafeArea(
+      child: Scaffold(
+        body: CustomScrollView(
+          controller: _scrollController,
+          slivers: <Widget>[
+            SliverAppBar(
+              floating: true, // makes the app bar hide and show on scroll
+              snap: true, // snap effects
+              automaticallyImplyLeading: false,
+              title: Image.asset('assets/while_transparent.png',
+                  width: 110 // Dynamic width for the image
+                  ),
+              elevation: 0,
+              backgroundColor: Colors.white,
+              actions: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 16.0),
+                  child: unreadNotifsAsyncValue.when(
+                    data: (int unreadCount) {
+                      if (unreadCount == 0) {
+                        return IconButton(
+                          icon: const Icon(Icons.notifications_none_rounded,
+                              color: Colors.black),
+                          onPressed: () async {
+                            await Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) =>
+                                    const NotificationsScreen()));
+                          },
+                        );
+                      } else {
+                        return IconButton(
+                          icon: const Icon(Icons.notifications_active_outlined,
+                              color: Colors.blueAccent),
+                          onPressed: () async {
+                            await Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) =>
+                                    const NotificationsScreen()));
+                          },
+                        );
+                      }
+                    },
+                    error: (error, stack) =>
+                        const Icon(Icons.error, color: Colors.red),
+                    loading: () => const Icon(Icons.notifications_none_rounded,
                         color: Colors.black),
-                    onPressed: () async {
-                      await Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => const NotificationsScreen()));
-                    },
-                  );
-                } else {
-                  return IconButton(
-                    icon: const Icon(Icons.notifications_active_outlined,
-                        color: Colors.blueAccent),
-                    onPressed: () async {
-                      await Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => const NotificationsScreen()));
-                    },
-                  );
-                }
-              },
-              error: (error, stack) =>
-                  const Icon(Icons.error, color: Colors.red),
-              loading: () => const Icon(Icons.notifications_none_rounded,
-                  color: Colors.black),
-            ),
-          ),
-        ],
-      ),
-      body: ListView.builder(
-        controller: _scrollController,
-        itemCount: categoriesState.categories.length +
-            (categoriesState.isLoading ? 1 : 0),
-        itemBuilder: (BuildContext context, int index) {
-          // Check if we're at the last item
-          if (index >= categoriesState.categories.length) {
-            // Show a loading indicator at the bottom
-            return const Center(child: CircularProgressIndicator());
-          }
-          // Display category item
-          log('list of categories length is ${categoriesState.categories.length}');
-
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(10, 7, 7, 0),
-                child: Text(
-                  categoriesState.categories[index],
-                  style: const TextStyle(fontSize: 17),
+                  ),
                 ),
+              ],
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  // Check if we're at the last item
+                  if (index >= categoriesState.categories.length) {
+                    // Show a loading indicator at the bottom
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  // Display category item
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 7, 7, 0),
+                        child: Text(
+                          categoriesState.categories[index],
+                          style: const TextStyle(fontSize: 17),
+                        ),
+                      ),
+                      FeedScreenWidget(
+                          category: categoriesState.categories[index]),
+                    ],
+                  );
+                },
+                childCount: categoriesState.categories.length +
+                    (categoriesState.isLoading ? 1 : 0),
               ),
-              FeedScreenWidget(category: categoriesState.categories[index]),
-            ],
-          );
-        },
+            ),
+          ],
+        ),
       ),
     );
   }
