2924ca84c1138b6377b982eb88d8b9491e0c5593
Anandmishra7946 <anandmishra7946@gmail.com>
2024-04-03 18:11:33 +0530

    foolowers bug

--- lib/feature/profile/screens/user_profile_follower_screen.dart
+++ lib/feature/profile/screens/user_profile_follower_screen.dart
@@ -1,119 +1,181 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:com.while.while_app/feature/auth/controller/auth_controller.dart';
+import 'package:com.while.while_app/feature/profile/screens/friend_profile_screen%20copy.dart';
+import 'package:com.while.while_app/main.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:com.while.while_app/providers/apis.dart';
 import 'package:com.while.while_app/core/utils/dialogs/dialogs.dart';
 import 'package:com.while.while_app/data/model/chat_user.dart';
+import 'package:com.while.while_app/feature/social/screens/chat/profile_dialog.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:get/get.dart';
 
-class UserProfileFollowerScreen extends ConsumerWidget {
-  const UserProfileFollowerScreen({super.key});
+//home screen -- where all available contacts are shown
+class UserProfileFollowerScreen extends ConsumerStatefulWidget {
+  const UserProfileFollowerScreen({super.key, required this.chatUser});
+  final ChatUser chatUser;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final fireService = ref.read(apisProvider);
-    final user = ref.watch(userProvider);
-    if (user == null) {
-      return Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back, color: Colors.black),
-            onPressed: () => Navigator.of(context).pop(),
-          ),
-          title: const Text('Follower', style: TextStyle(color: Colors.black)),
-          backgroundColor: Colors.white,
-        ),
-        body: const Center(child: Text('User not found')),
-      );
-    }
+  ConsumerState<UserProfileFollowerScreen> createState() =>
+      UserProfileFollowerScreenState();
+}
 
+class UserProfileFollowerScreenState
+    extends ConsumerState<UserProfileFollowerScreen> {
+  // for storing search status
+  List<ChatUser> _list = [];
+
+  @override
+  Widget build(BuildContext context) {
+    final fireService = ref.read(apisProvider);
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         automaticallyImplyLeading: false,
         leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.black),
-          onPressed: () => Navigator.of(context).pop(),
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
         ),
-        title: const Text('Follower', style: TextStyle(color: Colors.black)),
+        title: const Text(
+          'Following',
+          style: TextStyle(color: Colors.black),
+        ),
         backgroundColor: Colors.white,
       ),
+
+      //body
       body: SafeArea(
-        child: StreamBuilder<QuerySnapshot>(
-          stream: FirebaseFirestore.instance
-              .collection('users')
-              .doc(user.id)
-              .collection('follower')
-              .snapshots(),
-          builder: (context, followerSnapshot) {
-            if (followerSnapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            }
-            if (!followerSnapshot.isNull) {
-              return const Center(child: Text('No followers found.'));
-            }
-            if (!followerSnapshot.hasData) {
-              return const Center(child: Text('No followers found.'));
-            }
-            final followerIds =
-                followerSnapshot.data!.docs.map((doc) => doc.id).toList();
-            return StreamBuilder<QuerySnapshot>(
-              stream: fireService.getAllUsers(followerIds),
-              builder: (context, usersSnapshot) {
-                if (usersSnapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
-                }
-                if (!usersSnapshot.hasData) {
-                  return const Center(child: Text('No users found.'));
-                }
+        child: StreamBuilder(
+          // stream: APIs.getFriendsUsersId(widget.chatUser),
+          stream: fireService.getFriendsFollowersUsersId(widget.chatUser),
 
-                final users = usersSnapshot.data!.docs
-                    .map((doc) =>
-                        ChatUser.fromJson(doc.data() as Map<String, dynamic>))
-                    .toList();
+          //get id of only known users
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              //if data is loading
+              case ConnectionState.waiting:
+              case ConnectionState.none:
+                return const Center(child: CircularProgressIndicator());
 
-                if (users.isEmpty) {
-                  return const Center(child: Text('No Connections Found!'));
-                }
+              //if some or all data is loaded then show it
+              case ConnectionState.active:
+              case ConnectionState.done:
+                return StreamBuilder(
+                  stream: fireService.getAllUsers(
+                      snapshot.data?.docs.map((e) => e.id).toList() ?? []),
 
-                return ListView.builder(
-                  itemCount: users.length,
-                  itemBuilder: (context, index) {
-                    final person = users[index];
-                    return ListTile(
-                      leading: ClipRRect(
-                        borderRadius: BorderRadius.circular(
-                            50), // Adjust based on your layout needs
-                        child: CachedNetworkImage(
-                          imageUrl: person.image,
-                          width: 50,
-                          height: 50,
-                          fit: BoxFit.cover,
-                          placeholder: (context, url) =>
-                              const CircularProgressIndicator(),
-                          errorWidget: (context, url, error) =>
-                              const CircleAvatar(
-                                  child: Icon(CupertinoIcons.person)),
-                        ),
-                      ),
-                      title: Text(person.name),
-                      subtitle: Text(person.email),
-                      trailing: ElevatedButton(
-                        onPressed: () async {
-                          // await fireService.removeFollower(user.id, person.id); // Assuming you have this functionality
-                          Dialogs.showSnackbar(context, 'Follower Removed');
-                        },
-                        child: const Text('Remove',
-                            style: TextStyle(color: Colors.black)),
-                      ),
-                    );
+                  //get only those user, who's ids are provided
+                  builder: (context, snapshot) {
+                    switch (snapshot.connectionState) {
+                      //if data is loading
+                      case ConnectionState.waiting:
+                      case ConnectionState.none:
+                      // return const Center(
+                      //     child: CircularProgressIndicator());
+
+                      //if some or all data is loaded then show it
+                      case ConnectionState.active:
+                      case ConnectionState.done:
+                        final data = snapshot.data?.docs;
+                        _list = data
+                                ?.map((e) => ChatUser.fromJson(e.data()))
+                                .toList() ??
+                            [];
+
+                        if (_list.isNotEmpty) {
+                          return ListView.builder(
+                            itemCount: _list.length,
+                            itemBuilder: (context, index) {
+                              final person = _list[index];
+
+                              return InkWell(
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (_) => FriendProfileScreen(
+                                                chatUser: person,
+                                              )));
+                                },
+                                child: ListTile(
+                                  leading: InkWell(
+                                    onTap: () {
+                                      showDialog(
+                                          context: context,
+                                          builder: (_) =>
+                                              ProfileDialog(user: person));
+                                    },
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(
+                                          mq.height * .03),
+                                      child: CachedNetworkImage(
+                                        width: mq.height * .055,
+                                        fit: BoxFit.fill,
+                                        height: mq.height * .055,
+                                        imageUrl: person.image,
+                                        errorWidget: (context, url, error) =>
+                                            const CircleAvatar(
+                                                child: Icon(
+                                                    CupertinoIcons.person)),
+                                      ),
+                                    ),
+                                  ),
+                                  title: Text(
+                                    person.name,
+                                    style: const TextStyle(color: Colors.black),
+                                  ),
+                                  subtitle: Text(
+                                    person.email,
+                                    style: const TextStyle(
+                                        color:
+                                            Colors.black38),
+                                  ),
+                                  trailing: TextButton(
+                                    style: TextButton.styleFrom(
+                                      backgroundColor: Colors.blueAccent,
+                                      elevation: 2,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(8),
+                                      ),
+                                    ),
+                                    onPressed: () async {
+                                      await fireService
+                                          .unfollow(person.id)
+                                          .then((value) {
+                                        if (value) {
+                                          Dialogs.showSnackbar(
+                                              context, 'Unfollowed');
+                                        }
+                                      });
+                                    },
+                                    child: const Text(
+                                      'Unfollow',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight
+                                            .bold, // Adjust the font weight as needed
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                          );
+                        } else {
+                          return const Center(
+                            child: Text('You are not following Anyone!',
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.white)),
+                          );
+                        }
+                    }
                   },
                 );
-              },
-            );
+            }
           },
         ),
       ),
