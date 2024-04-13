8513c21ea64460db49f9f9acc8de274403e1aaf8
Arjun Saxena <rabbit.404.saxena@gmail.com>
2024-04-13 22:16:08 +0530

    community list already then then update done

--- lib/feature/social/screens/community/resources/avail_users_dialog.dart
+++ lib/feature/social/screens/community/resources/avail_users_dialog.dart
@@ -1,43 +1,49 @@
 import 'dart:developer';
+
 import 'package:com.while.while_app/data/model/chat_user.dart';
 import 'package:com.while.while_app/providers/apis.dart';
 import 'package:com.while.while_app/providers/connect_users_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class UserListDialog extends ConsumerWidget {
+class UserListDialog extends ConsumerStatefulWidget {
   final String commId;
-
   final List<ChatUser> list;
 
-  const UserListDialog({super.key, required this.commId, required this.list});
+  const UserListDialog({Key? key, required this.commId, required this.list})
+      : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    // final toggle = ref.watch(toggleSearchStateProvider);
+  _UserListDialogState createState() => _UserListDialogState();
+}
+
+class _UserListDialogState extends ConsumerState<UserListDialog> {
+  @override
+  Widget build(BuildContext context) {
+    log("Building UserListDialog");
     final allUsersAsyncValue = ref.watch(allUsersProvider);
-    final followingUsersAsyncValue =
-        ref.watch(communityParticipantsProvider(commId));
+    log(widget.list.length.toString());
+    final communityParticipantsAsyncValue =
+        ref.watch(communityParticipantsProvider(widget.commId));
 
     return Scaffold(
+      appBar: AppBar(title: const Text('Select a User')),
       body: allUsersAsyncValue.when(
-        data: (allUsers) => followingUsersAsyncValue.when(
-          data: (followingUsers) {
-            log('list of users ${followingUsers.toString()}');
-            final nonFollowingUsers = allUsers
-                .where((user) => !followingUsers.contains(user.id))
+        data: (allUsers) => communityParticipantsAsyncValue.when(
+          data: (communityUsers) {
+            final usersNotInCommunity = allUsers
+                .where((user) => !communityUsers.contains(user.id))
                 .toList();
-            log('length of all participants ${followingUsers.length}');
+            log("Users not in the community: ${usersNotInCommunity.map((u) => u.name).join(', ')}");
 
-            log('length of all users ${allUsers.length}');
-
-            log('length of non participants users ${nonFollowingUsers.length}');
+            if (usersNotInCommunity.isEmpty) {
+              return const Center(child: Text("No users available to add."));
+            }
 
             return ListView.builder(
-              itemCount: nonFollowingUsers.length,
+              itemCount: usersNotInCommunity.length,
               itemBuilder: (context, index) {
-                final user = nonFollowingUsers[index];
-
+                final user = usersNotInCommunity[index];
                 return ListTile(
                   leading: CircleAvatar(
                     backgroundImage: NetworkImage(user.image),
@@ -46,12 +52,11 @@ class UserListDialog extends ConsumerWidget {
                   subtitle: Text(user.email),
                   trailing: ElevatedButton(
                     onPressed: () async {
-                      // final didFollow = await ref.read(followUserProvider)(
-                      //     APIs.me.id, user.id);
-
+                      log("Adding User: ${user.id} to community");
                       await ref
-                          .read(apisProvider)
-                          .adminAddUserToCommunity(commId, user);
+                          .watch(apisProvider)
+                          .adminAddUserToCommunity(widget.commId, user);
+                      setState(() {}); // This forces the widget to rebuild
                     },
                     child: const Text('Add'),
                   ),
