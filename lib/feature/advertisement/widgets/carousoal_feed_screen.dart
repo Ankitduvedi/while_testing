37d4d96b0c5b581c795cac6bdcedb1e28f65e2b6
ANKIT KUMAR DUVEDI <ankitduvedi.03@gmail.com>
2024-04-15 18:09:28 +0530

    Advertisement

--- lib/feature/advertisement/widgets/carousoal_feed_screen.dart
+++ lib/feature/advertisement/widgets/carousoal_feed_screen.dart
@@ -1,8 +1,11 @@
+import 'dart:developer';
+
 import 'package:com.while.while_app/feature/advertisement/models/carousel_feed_scren_model.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ImageCarousel extends StatefulWidget {
   const ImageCarousel({super.key});
@@ -15,6 +18,14 @@ class ImageCarouselState extends State<ImageCarousel> {
 
   @override
   Widget build(BuildContext context) {
+    Future<void> _launchUrl(String url) async {
+      log('function clicked');
+      if (!await launchUrl(Uri.parse(url))) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text('Could not launch $url')));
+      }
+    }
+
     return StreamBuilder(
       stream: firestore
           .collection('advertisement')
@@ -44,52 +55,55 @@ class ImageCarouselState extends State<ImageCarousel> {
           items: data.map((url) {
             return Builder(
               builder: (BuildContext context) {
-                return Card(
-                  color: Colors.black,
-                  elevation: 4.0,
-                  child: Stack(
-                    alignment: AlignmentDirectional.bottomStart,
-                    children: [
-                      ClipRRect(
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(10.0)),
-                        child: CachedNetworkImage(
-                          imageUrl: url.thumbnail,
-                          fit: BoxFit.cover,
-                          placeholder: (context, url) =>
-                              const Center(child: CircularProgressIndicator()),
-                          errorWidget: (context, url, error) =>
-                              const Icon(Icons.error),
+                return GestureDetector(
+                  onTap: () => _launchUrl(url.website),
+                  child: Card(
+                    color: Colors.black,
+                    elevation: 4.0,
+                    child: Stack(
+                      alignment: AlignmentDirectional.bottomStart,
+                      children: [
+                        ClipRRect(
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(10.0)),
+                          child: CachedNetworkImage(
+                            imageUrl: url.thumbnail,
+                            fit: BoxFit.cover,
+                            placeholder: (context, url) => const Center(
+                                child: CircularProgressIndicator()),
+                            errorWidget: (context, url, error) =>
+                                const Icon(Icons.error),
+                          ),
                         ),
-                      ),
-                      Positioned.fill(
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            gradient: LinearGradient(
-                              begin: Alignment.center,
-                              end: Alignment.bottomCenter,
-                              colors: [
-                                Colors.transparent,
-                                Colors.black.withOpacity(0.2),
-                                Colors.black.withOpacity(0.4),
-                                Colors.black.withOpacity(0.9),
-                              ],
+                        Positioned.fill(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              gradient: LinearGradient(
+                                begin: Alignment.center,
+                                end: Alignment.bottomCenter,
+                                colors: [
+                                  Colors.transparent,
+                                  Colors.black.withOpacity(0.2),
+                                  Colors.black.withOpacity(0.4),
+                                  Colors.black.withOpacity(0.9),
+                                ],
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          'Ad. ${url.organisation}',
-                          maxLines: 1,
-                          style: const TextStyle(
-                              fontSize: 16.0,
-                              color: Color.fromARGB(255, 255, 255, 255)),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            'Ad. ${url.organisation}',
+                            maxLines: 1,
+                            style: const TextStyle(
+                                fontSize: 16.0,
+                                color: Color.fromARGB(255, 255, 255, 255)),
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 );
               },
