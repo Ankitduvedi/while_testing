e1eaf0641d998e2a509cce75577245bd5fa6d7c0
arpitvermaofficial <arpitv747@gmail.com>
2024-06-15 18:00:05 +0530

    BUG: New profile screen

--- lib/feature/profile/screens/user_profile_screen2.dart
+++ lib/feature/profile/screens/user_profile_screen2.dart
@@ -11,9 +11,7 @@ import 'package:com.while.while_app/feature/profile/screens/user_leaderboard_scr
 import 'package:com.while.while_app/feature/profile/screens/creator_profile_widget.dart';
 import 'package:com.while.while_app/feature/profile/screens/profile_data_widget2.dart';
 import 'package:tutorial_coach_mark/tutorial_coach_mark.dart';
-
 import '../../../core/constant.dart';
-import '../../../providers/user_provider.dart';
 import '../../auth/controller/auth_controller.dart';
 
 class ProfileScreen extends ConsumerStatefulWidget {
@@ -24,12 +22,6 @@ class ProfileScreen extends ConsumerStatefulWidget {
 }
 
 class _ProfileScreenState extends ConsumerState<ProfileScreen> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-  }
-
   final photosKey = GlobalKey();
   final profileKey = GlobalKey();
   final statsKey = GlobalKey();
@@ -41,7 +33,7 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
         photosKey: photosKey, profileKey: profileKey, statsKey: statsKey);
     tutorialCoachMark = TutorialCoachMark(
       targets: targets,
-      colorShadow: Colors.blueAccent,
+      colorShadow: const Color.fromARGB(255, 44, 121, 255),
       hideSkip: false,
       textSkip: 'SKIP',
       paddingFocus: 10,
@@ -71,12 +63,8 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     final user = ref.read(userDataProvider).userData;
-    print("user id1: ${user?.id}");
 
     // ref.watch(userDataProvider);
-    print("containing ${user?.name}");
-
-    log("uid ${user?.id} ");
 
     if (user?.id != null &&
         user?.id != "" &&
@@ -89,97 +77,167 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
     var tabBarIcons = [
       Tab(
         key: photosKey,
-        icon: Icon(
-          Icons.photo_outlined,
-          color: Colors.blueGrey,
-          size: 30,
-        ),
+        text: 'Loops',
       ),
       Tab(
         key: profileKey,
-        icon: Icon(
-          Icons.person,
-          color: Colors.blueGrey,
-          size: 30,
-        ),
+        text: 'Videos',
       ),
       Tab(
         key: statsKey,
-        icon: Icon(
-          Icons.brush,
-          color: Colors.blueGrey,
-          size: 30,
-        ),
+        text: 'Dashboard',
       ),
     ];
 
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
+    return SafeArea(
+      child: Scaffold(
+        // appBar: AppBar(
+        //   automaticallyImplyLeading: false,
+        //   backgroundColor: Colors.white,
+        //   title: Text(
+        //     user!.name,
+        //     style: GoogleFonts.ptSans(color: Colors.black),
+        //   ),
+        //   actions: [
+        //     IconButton(
+        //       iconSize: 25,
+        //       onPressed: () {
+        //         showModalBottomSheet(
+        //           context: context,
+        //           builder: (context) {
+        //             return const MoreOptions();
+        //           },
+        //         );
+        //       },
+        //       icon: const Icon(
+        //         Icons.settings,
+        //         color: Colors.black,
+        //       ),
+        //     ),
+        //   ],
+        // ),
         backgroundColor: Colors.white,
-        title: Text(
-          user?.name ?? "",
-          style: GoogleFonts.ptSans(color: Colors.black),
-        ),
-        actions: [
-          IconButton(
-            iconSize: 35,
-            onPressed: () {
-              showModalBottomSheet(
-                context: context,
-                builder: (context) {
-                  return const MoreOptions();
-                },
-              );
-            },
-            icon: const Icon(
-              Icons.more_vert,
-              color: Colors.black,
-            ),
-          ),
-        ],
-      ),
-      backgroundColor: Colors.white,
-      body: DefaultTabController(
-        length: 3,
-        child: NestedScrollView(
-          headerSliverBuilder: (context, _) => [
-            SliverList(
-              delegate: SliverChildListDelegate(
-                [const ProfileDataWidget()],
-              ),
-            ),
-          ],
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Material(
-                color: Colors.white,
-                child: TabBar(
-                  padding: EdgeInsets.all(0),
-                  indicatorColor: Colors.black,
-                  tabs: tabBarIcons,
-                ),
-              ),
-              Expanded(
-                child: TabBarView(
-                  children: [
-                    Center(
-                        child: CreatorProfile(
-                      user: user!,
-                    )),
-                    Center(
-                        child: CreatorProfileVideo(
-                      user: user!,
-                    )),
-                    const Center(child: LeaderboardScreen()),
-                  ],
+        body: DefaultTabController(
+          length: 3,
+          child: NestedScrollView(
+            headerSliverBuilder: (context, _) => [
+              SliverList(
+                delegate: SliverChildListDelegate(
+                  [const ProfileDataWidget()],
                 ),
               ),
             ],
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Material(
+                  color: Colors.white,
+                  child: TabBar(
+                      padding: const EdgeInsets.all(0),
+                      //indicatorColor: const Color.fromARGB(255, 131, 85, 205),
+                      labelColor: const Color.fromARGB(255, 196, 68, 216),
+                      unselectedLabelColor: Colors.black,
+                      labelStyle: GoogleFonts.urbanist(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 18,
+                      ),
+                      unselectedLabelStyle: GoogleFonts.urbanist(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18,
+                      ),
+                      tabs: tabBarIcons,
+                      indicator: CurvedGradientTabIndicator(
+                          gradient: const LinearGradient(
+                        colors: [
+                          Color.fromRGBO(230, 77, 255, 1),
+                          Color.fromRGBO(123, 68, 212, 1),
+                        ],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                      ))),
+                ),
+                Expanded(
+                  child: TabBarView(
+                    children: [
+                      Center(
+                          child: CreatorProfile(
+                        user: user!,
+                      )),
+                      Center(
+                          child: CreatorProfileVideo(
+                        user: user,
+                      )),
+                      const Center(child: LeaderboardScreen()),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+}
+
+class GradientText extends StatelessWidget {
+  final String text;
+  final Gradient gradient;
+  final TextStyle style;
+
+  const GradientText({
+    required this.text,
+    required this.gradient,
+    required this.style,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ShaderMask(
+      shaderCallback: (bounds) => gradient.createShader(
+        Rect.fromLTWH(0, 0, bounds.width, bounds.height),
+      ),
+      child: Text(
+        text,
+        style: GoogleFonts.urbanist(
+            fontWeight: FontWeight.w600, fontSize: 20, color: Colors.white),
+      ),
+    );
+  }
+}
+
+class CurvedGradientTabIndicator extends Decoration {
+  final BoxPainter _painter;
+
+  CurvedGradientTabIndicator({required Gradient gradient})
+      : _painter = _CurvedGradientPainter(gradient);
+
+  @override
+  BoxPainter createBoxPainter([VoidCallback? onChanged]) => _painter;
+}
+
+class _CurvedGradientPainter extends BoxPainter {
+  final Gradient gradient;
+
+  _CurvedGradientPainter(this.gradient);
+
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
+    final Paint paint = Paint()
+      ..shader = gradient.createShader(
+        Rect.fromLTWH(
+          offset.dx,
+          configuration.size!.height - 4, // Increased height
+          configuration.size!.width * 1.4, // Reduced width
+          12, // Increased height
+        ),
+      );
+    final Rect rect = Offset(
+          offset.dx + (configuration.size!.width * -.2), // Centered
+          offset.dy + configuration.size!.height - 4, // Adjusted height
+        ) &
+        Size(configuration.size!.width * 1.4, 4); // Adjusted size
+    final RRect rrect = RRect.fromRectAndRadius(rect, Radius.circular(16));
+    canvas.drawRRect(rrect, paint);
   }
 }
